1392f1bc7719b8645f61bd1262f0fe0c08a7eae7
Satyabrat <satyabrat34@gmail.com>
2026-05-19 11:08:09 +0530

    secure cookie setting utility

--- backend/src/lib/generateToken.js
+++ backend/src/lib/generateToken.js
@@ -5,11 +5,13 @@ const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
       expiresIn: "15d",
     });
+    const isProduction = process.env.NODE_ENV === "production";
+    
     res.cookie("jwt", token, {
       maxAge: 15 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "none",
-      secure: true,
+      sameSite: isProduction ? "none" : "strict",
+      secure: isProduction,
     });
     return token;
   } catch (error) {
